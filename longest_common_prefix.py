45d4015b1ae84fe7305887ea3a7848129ee6c4fc
Kevin Schenthal <kevinschenthal@macintosh-3.attlocal.net>
2018-10-26 17:44:57 -0700

    longest common prefix solution.

---
+++ longest_common_prefix.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 26 04:47:01 2018
+
+@author: kevinschenthal
+"""
+
+def longestCommonPrefix(strs):
+    """
+    :type strs: List[str]
+    :rtype: str
+    """
+    
+    longest_prefix = ''
+    l = 1
+    while l > 0:
+        prefixes = [x[:l] for x in strs]
+        if len(set(prefixes)) == 1:
+            if longest_prefix == prefixes[0]:  # Handle null input ['']
+                break
+            longest_prefix = prefixes[0]
+            l += 1
+        else:
+            l = -1
+    return longest_prefix
