ebe90c10f75fd0faa9f1c71abc72f3d858117560
mathvolcano <mathvolcano@users.noreply.github.com>
2021-02-24 11:22:25 -0800

    572

---
+++ 572_isSubtree.py
@@ -0,0 +1,28 @@
+"""
+572. Subtree of Another Tree
+https://leetcode.com/problems/subtree-of-another-tree/
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, s, t):
+        if not s and not t:
+            return True
+        if not s or not t:
+            return False
+        if s.val != t.val:
+            return False
+        else:
+            return self.isSameTree(s.left, t.left) and self.isSameTree(s.right, t.right)
+
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        if not s and not t:
+            return True
+        if not s or not t:
+            return False
+        return self.isSameTree(s, t) or self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
