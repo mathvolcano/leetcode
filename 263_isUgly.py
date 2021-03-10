f3cba7552159b5d20db1ffa06c05978f0da26d52
mathvolcano <mathvolcano@users.noreply.github.com>
2021-03-10 08:10:01 -0800

    Create 263 isUgly

---
+++ 263_isUgly.py
@@ -0,0 +1,18 @@
+"""
+263. Ugly Number
+https://leetcode.com/problems/ugly-number/
+"""
+
+class Solution:
+    def isUgly(self, num: int) -> bool:
+        if num <= 0: return False
+        while num > 1:
+            if num % 2 == 0:
+                num = num / 2
+            elif num % 3 == 0:
+                num = num / 3
+            elif num % 5 == 0:
+                num = num / 5
+            else:
+                return False
+        return True
