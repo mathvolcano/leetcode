ac433fd99e3d46dd2fddc08ef134b4f8adb5c236
mathvolcano <mathvolcano@users.noreply.github.com>
2021-03-18 09:13:03 -0700

    Create 700_searchBST.py

---
+++ 700_searchBST.py
@@ -0,0 +1,20 @@
+"""
+700. Search in a Binary Search Tree
+https://leetcode.com/problems/search-in-a-binary-search-tree/
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root: return
+        if root.val == val: return root
+        if val < root.val and root.left:
+            return self.searchBST(root.left, val)
+        if val > root.val and root.right:
+            return self.searchBST(root.right, val)
+        return
