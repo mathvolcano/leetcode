d6568dc55436425b40d995fd2bdb61264e414e53
mathvolcano <mathvolcano@users.noreply.github.com>
2021-04-07 09:50:35 -0700

    Simplify header in 91_numDecodings

--- 91_numDecodings.py
+++ 91_numDecodings.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Wed Mar 18 10:00:26 2020
-
-@author: mathvolcano
-
 91. Decode Ways
 https://leetcode.com/problems/decode-ways/
 """
