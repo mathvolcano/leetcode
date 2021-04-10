07974ad2af388330b61e10cfbc7da45d15dada2f
mathvolcano <mathvolcano@users.noreply.github.com>
2021-04-10 08:53:51 -0700

    Add binary search solution to 852_peakIndexInMountainArray

--- 852_peakIndexInMountainArray.py
+++ 852_peakIndexInMountainArray.py
@@ -4,4 +4,17 @@ https://leetcode.com/problems/peak-index-in-a-mountain-array/
 """
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        return arr.index(max(arr))
+        # Because the arr is guranteed to have a mount, it is combined 2 sorted arrays.
+        # So perform a binary search
+        l, r = 0, len(arr) - 1
+        while l < r:
+            m = (l + r) //2
+            if (arr[m-1] < arr[m] > arr[m+1]):
+                return m
+            elif (arr[m-1] > arr[m]):
+                r = m - 1
+            else:
+                l = m + 1
+
+        # Pythonic O(n) time say
+        # return arr.index(max(arr))
