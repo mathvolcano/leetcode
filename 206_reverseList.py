bc7f9e6028c67ee1f0ccc7cc88a2f060823cdd2b
mathvolcano <mathvolcano@users.noreply.github.com>
2021-04-10 17:28:45 -0700

    Add recursive solution to 206_reverseList.py

--- 206_reverseList.py
+++ 206_reverseList.py
@@ -1,27 +1,36 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Wed Feb 26 07:33:44 2020
-
-@author: mathvolcano
-
-https://leetcode.com/problems/reverse-linked-list/
-
 206. Reverse Linked List
+https://leetcode.com/problems/reverse-linked-list/
 """
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        # Both ways are O(n) time and O(n) space
 
-def reverseList(head) -> ListNode:
-        
-    # Get reversed values
-    n = head
-    new_list = []
-    while n:
-        new_list.insert(0, n.val)
-        n = n.next
-        
-    # Change values in place
-    r = head
-    for v in new_list:
-        r.val = v
-        r = r.next
-    return head
+        ### Recursive Solution
+        if not head or (not head.next): return head
+
+        # Recurse
+        start = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return start
+
+        ### Iterative Solution
+        # Get reversed values
+        n = head
+        new_list = []
+        while n:
+            new_list.insert(0, n.val)
+            n = n.next
+
+        # Change values in place
+        r = head
+        for v in new_list:
+            r.val = v
+            r = r.next
+        return head
