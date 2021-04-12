0284e17dd8bcb61c002ea3c619abcd186204dfc4
mathvolcano <mathvolcano@users.noreply.github.com>
2021-04-12 08:36:40 -0700

    Refactor climbStairs.py

--- climbStairs.py
+++ climbStairs.py
@@ -1,23 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Mon Nov 12 07:27:30 2018
-
-@author: mathvolcano
-
-Climb Stairs
+70. Climbing Stairs
+https://leetcode.com/problems/climbing-stairs/
 """
 
 
 class Solution:
-    def climbStairs(self, n):
-        # boundary case
-        if n in (0, 1):
-            return n
-        
-        n_ways = [0] * (n + 1)
-        n_ways[0] = n_ways[1] = 1
-        for i in range(2, n + 1):
-            n_ways[i] = n_ways[i - 1] + n_ways[i - 2]
-        return n_ways[n]
-    
+    def climbStairs(self, n: int) -> int:
+        if n <= 3: return n
+
+        dp = [0] * (n)
+        dp[:3] = [1,2,3]
+
+        for i in range(3,n):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[-1]
