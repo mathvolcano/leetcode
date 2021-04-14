a88a96e4d2e27444258a3f667e00c93949b5dafd
mathvolcano <mathvolcano@users.noreply.github.com>
2021-04-14 16:17:26 -0700

    Refactor 1_twoSum.py

--- 1_twoSum.py
+++ 1_twoSum.py
@@ -5,11 +5,6 @@ https://leetcode.com/problems/two-sum/
 
 class Solution:
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
         n_to_index = {}
         for i, n in enumerate(nums):
             if target - n in n_to_index:
