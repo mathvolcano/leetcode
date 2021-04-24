9f85ac48ee6f13680fb63ddba73fabbbed810330
mathvolcano <mathvolcano@users.noreply.github.com>
2021-04-24 14:09:49 -0700

    Clean up 139_wordBreak.py

--- 139_wordBreak.py
+++ 139_wordBreak.py
@@ -3,29 +3,18 @@
 https://leetcode.com/problems/word-break/
 """
 
-def wordBreak(s, wordDict):
-    """DP: O(n*m)."""
-    n = len(s)
-    if n == 0: return True
-    if s in wordDict: return True
-    
-    valid = {0: True}
-    for i in range(1, n+1):
-        valid[i] = False
-        for j in range(i):
-            if valid[j] and (s[j:i] in wordDict):
-                valid[i] = True
-                break
-    return valid[n]
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # DP O(n*m)
+        n = len(s)
+        if n == 0: return True
+        if s in wordDict: return True
 
-s = "leetcode"
-wordDict = ["leet", "code"]
-wordBreak(s, wordDict)
-
-s = "applepenapple"
-wordDict = ["apple", "pen"]
-wordBreak(s, wordDict)
-
-s = "catsandog"
-wordDict = ["cats", "dog", "sand", "and", "cat"]
-wordBreak(s, wordDict)
+        valid = {0: True}
+        for i in range(1, n+1):
+            valid[i] = False
+            for j in range(i):
+                if valid[j] and (s[j:i] in wordDict):
+                    valid[i] = True
+                    break
+        return valid[n]
