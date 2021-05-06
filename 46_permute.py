421be91621855c9b23809c68c32b42154e75eaec
mathvolcano <mathvolcano@users.noreply.github.com>
2021-05-06 08:43:29 -0700

    Add better DFS solution for 46_permute.py

--- 46_permute.py
+++ 46_permute.py
@@ -3,15 +3,40 @@
 https://leetcode.com/problems/permutations/
 """
 
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # [1] Given all the permutation of a subarray, perms_sub, add the next element after the
+        # perms_sub to sub to get a new all the new permutations that include the next element
+        # e.g., perms_sub + [perms_sub + [current]]
+        # [2] Perform a DFS iterating through nums
+        # O(n!) time and space.
 
-def permute(nums):
-    if len(nums) == 1: return [nums]
-    if len(nums) == 2: return [[nums[0], nums[1]],
-                               [nums[1], nums[0]]]
+        def helper(arr, current, permutations):
+            if not len(arr) and len(current):
+                permutations.append(current)
+            else:
+                for i in range(len(arr)):
+                    newArr = arr[:i] + arr[i+1:]
+                    newCurr = current + [arr[i]]
+                    helper(newArr, newCurr, permutations)
+
+        result = []
+        helper(nums, [], result)
+        return result
+
+
+
+
+
+    # # Base Case
+    # if len(nums) == 1: return [nums]
+    # if len(nums) == 2: return [[nums[0], nums[1]],
+    #                            [nums[1], nums[0]]]
 
     # Factory method
     # import itertools
     # return list(itertools.permutations(nums))
+    # Recursion
     results = []
     for i in range(len(nums)):
         n = nums[i]
@@ -20,9 +45,3 @@ def permute(nums):
         perms = [[n] + p for p in non_n_perm]
         results = results + perms
     return results
-
-
-
-
-nums = [1,2,3]
-print(permute(nums))
