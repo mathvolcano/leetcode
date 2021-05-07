9a84cda6d94725805a5b314e3121015408ec90f3
mathvolcano <mathvolcano@users.noreply.github.com>
2021-05-07 10:05:58 -0700

    Simplify 583_minDistance

--- 583_minDistance.py
+++ 583_minDistance.py
@@ -1,33 +1,33 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Sun Mar  8 14:24:58 2020
-
-@author: mathvolcano
-
 583. Delete Operation for Two Strings
 https://leetcode.com/problems/delete-operation-for-two-strings/
 """
 
-def minDistance(word1, word2):
-    rows = len(word1) +1
-    cols = len(word2) + 1
-    if rows == 1 or cols == 1:
-        return max(rows, cols)
-    
-    matrix = [[0] * (cols) for i in range(rows)]
-    
-    for r in range(rows):
-        for c in range(cols):
-            if r == 0 or c == 0:
-                matrix[r][c] = r + c
-            elif word1[r-1] == word2[c-1]:
-                matrix[r][c] = matrix[r-1][c-1]
-            else:
-                matrix[r][c] = min(matrix[r-1][c], matrix[r][c-1]) + 1
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        # Dynamic Programming
+        # Note that the min number of deletes of word1 and word2 equals
+        # the number of deletes of word1[:-1] and word2[:-1] when word1[-1] == word2[-1]
+        # but requires 1 more if they aren't equal.
+        # [1] Create a matrix of size len(word1)+1, len(word2)+1
+        # so that the matrix (r,c) track the min number of deletes up to word1[r-1], word2[c-1].
+        # We need an extra row & column of 0s so that the lookback works.
+        # [2] Iterate through the matrix r row and c col
+        # Example1, the DP  atrix becomes
+        # 0 1 2 3
+        # 1 2 3 4
+        # 2 1 2 3
+        # 3 2 1 2
 
-    return matrix[-1][-1]
+        m, n = len(word1)+1, len(word2)+1
+        dp = [[0] * n for _ in range(m)]
 
-minDistance("sea", "eat") # 2
-
-minDistance('meet', 'greet') # 3
+        for r in range(m):
+            for c in range(n):
+                if r == 0 or c == 0:
+                    dp[r][c] = r + c
+                elif word1[r-1] == word2[c-1]:
+                    dp[r][c] = dp[r-1][c-1]
+                else:  # word1[r-1] != word2[c-1]
+                    dp[r][c] = min(dp[r][c-1], dp[r-1][c]) + 1
+        return dp[-1][-1]
