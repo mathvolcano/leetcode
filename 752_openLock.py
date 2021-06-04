2407371cb166025b6be6ba8ffa4288f93e8eb790
mathvolcano <mathvolcano@users.noreply.github.com>
2021-06-04 08:57:08 -0700

    Simplify 752_openLock to BFS and add complexity.

--- 752_openLock.py
+++ 752_openLock.py
@@ -3,105 +3,35 @@ Open the Lock
 https://leetcode.com/problems/open-the-lock/
 """
 
-def openLock(deadends, target):
-    initial_state = '0000'
-    
-    if initial_state == target:
-        return 0
-    if initial_state in deadends:
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+
+        # BFS
+        # [1] Create a queue to store the (states, step) after n steps starting from ('0000',0)
+        # [2a] Iterate through queue, popleft
+        # [2b] if state is in target then return steps
+        # [2c] else if not in deadends get neighbor states for the state
+        # [3] Check if neighbor states are already visited (hash set), if not append to queue
+        # [3] return -1 if queue empty
+
+        # Let n be the number of digits of the lock
+        # Space complexity is O(2^n) capped at total states so could be considered as O(1)
+        # Time complexity: O(10^n) worst case because would have to check all digits.
+
+        start = '0000'
+        if start in deadends: return -1
+        if start == target: return 0
+
+        from collections import deque
+        q = deque([(start, 0)])
+        visited = set([start])
+        while q:
+            cur, steps = q.popleft()
+            for i in range(4):
+                for j in [-1, 1]:
+                    nxt = cur[:i] + str((int(cur[i]) + j + 10) % 10) + cur[i+1:]
+                    if nxt == target: return steps + 1
+                    if nxt in deadends or nxt in visited: continue
+                    q.append((nxt, steps + 1))
+                    visited.add(nxt)
         return -1
-    
-    visited = set(initial_state)
-    
-    def get_1_move_states(state):
-        next_states = []
-        for i in range(4):
-            left, right = state[:i], state[i+1:]
-            prev = int(state[i]) - 1 if (int(state[i]) - 1 >= 0) else 9
-            nxt = int(state[i]) + 1 if (int(state[i]) + 1) < 10 else 0
-            new_state1 = left + str(prev) + right
-            new_state2 = left + str(nxt) + right
-            next_states.append(new_state1)
-            next_states.append(new_state2)
-        
-        return next_states  
-        
-    def not_deadend_states(states):
-        return [s for s in states if s not in deadends]
-    
-    def not_visited(states):
-        return [s for s in states if s not in visited]
-    
-    
-    # BFS
-    n_steps = 0
-    queue = get_1_move_states(initial_state)
-    while queue:
-        size = len(queue)
-        n_steps += 1
-        for _ in range(size):
-            state = queue.pop()
-            if state == target:
-                return n_steps
-            next_states = get_1_move_states(state)
-            next_states
-            for next_state in next_states:
-                if (next_state in deadends) or (next_state in visited):
-                    continue
-                visited.add(next_state)
-                queue.insert(0, next_state)
-                
-    return -1
-
-openLock(deadends=["8888"], target="0009")
-
-assert openLock(deadends=["8888"], target="0009") == 1
-assert openLock(deadends = ["0201","0101","0102","1212","2002"],
-                target = "0202") == 6
-assert openLock(deadends=["8887","8889","8878","8898","8788","8988","7888","9888"],
-                target = "8888") == -1
-assert openLock(deadends=["0000"], target="8888") == -1
-
-#class Solution:
-#    def get_1_move_states(self, state):
-#            next_states = []
-#            for i in range(4):
-#                left, right = state[:i], state[i+1:]
-#                prev = int(state[i]) - 1 if (int(state[i]) - 1 >= 0) else 9
-#                nxt = int(state[i]) + 1 if (int(state[i]) + 1) < 10 else 0
-#                new_state1 = left + str(prev) + right
-#                new_state2 = left + str(nxt) + right
-#                next_states.append(new_state1)
-#                next_states.append(new_state2)
-#
-#            return next_states  
-#    
-#    def openLock(self, deadends: List[str], target: str) -> int:
-#        initial_state = '0000'
-#    
-#        if initial_state == target:
-#            return 0
-#        if initial_state in deadends:
-#            return -1
-#
-#        visited = set(initial_state)
-#
-#        # BFS
-#        n_steps = 0
-#        queue = self.get_1_move_states(initial_state)
-#        while queue:
-#            size = len(queue)
-#            n_steps += 1
-#            for _ in range(size):
-#                state = queue.pop()
-#                if state == target:
-#                    return n_steps
-#                next_states = self.get_1_move_states(state)
-#                next_states
-#                for next_state in next_states:
-#                    if (next_state in deadends) or (next_state in visited):
-#                        continue
-#                    visited.add(next_state)
-#                    queue.insert(0, next_state)
-#
-#        return -1
