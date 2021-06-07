cbb13983ead33302faa4e34335d837d6982e09b0
mathvolcano <mathvolcano@users.noreply.github.com>
2021-06-07 14:24:09 -0700

    Rename 746_minCostClimbingStairs to 0 746_minCostClimbingStairs

--- 0746_minCostClimbingStairs.py
+++ 0746_minCostClimbingStairs.py
@@ -5,11 +5,11 @@ https://leetcode.com/problems/min-cost-climbing-stairs/
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        dp = cost
-        dp[:2] = cost[:2]
-        if n == 2: return min(cost)
 
-        for i in range(2,len(cost)):
+        # DP
+        # Note the cost of reaching step i is the min of the cost of
+        # reaching step i-1 or step i-2.
+        # Time complexity O(n), space complexity O(1) additional space
+        for i in range(2, len(cost)):
             cost[i] += min(cost[i-1], cost[i-2])
         return min(cost[-1], cost[-2])
