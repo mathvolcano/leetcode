9be9fc11853e1bb2ea07dabcf070dc57270ac69a
mathvolcano <mathvolcano@users.noreply.github.com>
2021-12-20 08:36:47 -0800

    Update 0215_findKthLargest.py

--- 0215_findKthLargest.py
+++ 0215_findKthLargest.py
@@ -5,23 +5,28 @@ https://leetcode.com/problems/kth-largest-element-in-an-array/
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        # Pythonic brute force: O(n log n) time, O(1) additional space
+        # if len(nums) == 0: return 0
+        # nums.sort(reverse=True)
+        # return nums[k-1]
 
+        # (Min) Heap: O(n log k) time complexity, O(k) additional space
+        from heapq import heappush, heappop
+        h, res = [], -float('inf')
+        for n in nums:
+            if len(h) < k:
+                heappush(h, n)
+            elif n > res:
+                heappush(h, n)
+                heappop(h)
+            res = min(h)
+        return res
 
-def findKthLargest(nums, k):
-    if len(nums) == 0:
-        return 0
-    
-    nums.sort(reverse=True)
-    return nums[k-1]
-
-ex1 = [3,2,1,5,6,4]
-k1 = 2
-assert findKthLargest(ex1, k1) == 5
-
-ex2 = [3,2,3,1,2,4,5,5,6]
-k2 = 4
-assert findKthLargest(ex2, k2) == 4
-
-ex1 = [3,2,1,5,6,4]
-k1 = 2
-assert findKthLargest(ex1, k1) == 5
+        # Faster in Leetcode, but have to run more heappushes & slower in theory.
+        # from heapq import heappush, heappop
+        # heap = []
+        # for num in nums:
+        #     heappush(heap,-num)
+        # for i in range(k):
+        #     res = heappop(heap)
+        # return -res
