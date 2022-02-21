47916e30cd67cf8f21efac396db3905d2c384b40
mathvolcano <mathvolcano@users.noreply.github.com>
2022-02-21 11:51:31 -0800

    Add explanation 0572_isSubtree

--- 0572_isSubtree.py
+++ 0572_isSubtree.py
@@ -9,6 +9,17 @@ https://leetcode.com/problems/subtree-of-another-tree/
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# DFS
+# [1] Define a function isSameTree to test whether 2 trees are the same
+# [a] Test if roots are empty, return True
+# [b] if only 1 tree is empty then return False
+# [c] Recurse on left & right children
+# [2] Iterate through the tree t checking if s equals t
+# [a] if not, then check children of t.
+
+# O(max(n_s, n_t)) time complexity to traverse each of the n nodes of s & t.
+# O(max(n_s, n_t)) space complexity because the tail recursion requires storing all nodes in memory
 class Solution:
     def isSameTree(self, s, t):
         if not s and not t:
