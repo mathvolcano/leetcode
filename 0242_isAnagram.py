ae81886dfb91e2fc83bf2de8bae70af9bb78b8cb
mathvolcano <mathvolcano@users.noreply.github.com>
2022-03-03 15:31:53 -0800

    Add leading 0s to file names

--- 0242_isAnagram.py
+++ 0242_isAnagram.py
@@ -1,12 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Sun Mar  1 11:43:37 2020
-
-@author: mathvolcano
-
 242. Valid Anagram
-
 https://leetcode.com/problems/valid-anagram/
 """
 
