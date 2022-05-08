61c588a5d13c03bec5bb564fca0b1f118f0d3289
mathvolcano <mathvolcano@users.noreply.github.com>
2022-05-07 18:09:10 -0700

    Add non-sort solution to 0438

--- 0438_findAnagrams.py
+++ 0438_findAnagrams.py
@@ -1,31 +1,34 @@
 """
 438. Find All Anagrams in a String
+https://leetcode.com/problems/find-all-anagrams-in-a-string/
 """
 
-def findAnagrams(s, p):
-    if (len(s) == 0):
-        return None
-    if len(p) > len(s):
-        return None
-    
-    p_sorted = ''.join(sorted(p))
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        # Sliding window with array hashtable
+        # [0] Initialize
+        #.    a) Initialize 26 char zero arrays, ls & lp
+        #.    b) populate lp's char counts by counts in p
+        #.    c) initialize result array res = []
+        # [1] Traverse enumerate(s)
+        #.    a) add char counts of s to ls
+        #.    b) if i >= len(p) then subtract last char's count s[i-np] from ls's char position
+        #.    c) check if lp == ls and if so then append index of s (i - np + 1) to res
+        # Complexity: let ns = len(s) and np = len(p)
+        # Time: O(ns + np) to create hashtable lp and traverse s
+        # Space: O(1) if there are 26 chars.
+        ls = [0]*26
+        lp = [0]*26
 
-    grams = [''.join(sorted(s[i:i+len(p)]))
-             for i in range(len(s) - len(p) + 1)]
-    idx_matches = []
-    for i, g in enumerate(grams):
-        if g == p_sorted:
-            idx_matches.append(i)
-    if idx_matches:
-        return idx_matches
-    return idx_matches if idx_matches else None
-    
-    
+        for x in p:
+            lp[ord(x) - ord('a')] += 1
 
-s1 = "cbaebabacd"
-p1 = "abc"
-assert findAnagrams(s1, p1) == [0, 6]
-
-s2 = "abab"
-p2 = "ab"
-assert findAnagrams(s2, p2) == [0, 1, 2]
+        res = []
+        np = len(p)
+        for i, v in enumerate(s):
+            ls[ord(v) - ord('a')] += 1
+            if i >= np:
+                ls[ord(s[i-np]) - ord('a')] -= 1
+            if lp == ls:
+                res.append(i-np+1)
+        return res
