dc9ea91a16f90aca31beda35f520f8cfcd979f2d
mathvolcano <mathvolcano@users.noreply.github.com>
2022-05-20 08:47:46 -0700

    Update 0075_sortColors to have description & be cleaner

--- 0075_sortColors.py
+++ 0075_sortColors.py
@@ -3,26 +3,31 @@ https://leetcode.com/problems/sort-colors/
 75. Sort Colors
 """
 
-def sortColors(nums):
-    if len(nums) <= 1: return nums
-    
-    # smaller, middle, larger division mark the points that we know are 0, 1, 2
-    s, m, l = 0, 0, len(nums)
-    while m < l:
-        if nums[m] == 0:
-            nums[s], nums[m] = nums[m], nums[s]
-            s += 1
-            m += 1
-        elif nums[m] == 1:
-            m += 1
-        else:
-            nums[l-1], nums[m] = nums[m], nums[l-1]
-            l -= 1
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # 2 Pointers
+        # Goal: Move all 0s before l and all 2s after h.
+        # [0] Initialize low & high pointers: Set l, h = 0, len(nums) -1, and a tracking index i= 0
+        # [1] while i <= r
+        # [2] Case logic
+        #  a. if nums[i] == 0, swap nums[i] with nums[l] and increment l & i
+        #  b. if nums[i] == 2, swap nums[i] with nums[h] and decrement h
+        #  c. if nums[i] == 1, increment i
+        # O(n) time and O(1) space
 
-
-nums = [2,0,2,1,1,0]
-sortColors(nums)
-nums # [0,0,1,1,2,2]
-
-nums = [2,0,1]
-sortColors(nums)
+        n = len(nums)
+        i, l, h = 0, 0, n - 1
+        while i <= h:
+            if nums[i] == 0:
+                nums[i], nums[l] = nums[l], nums[i]
+                i += 1
+                l += 1
+            elif nums[i] == 1:
+                i += 1
+            else: # nums[i] == 2
+                nums[i], nums[h] = nums[h], nums[i]
+                h -= 1
+        return
