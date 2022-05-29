cafb8c61a8b7182b36c617ec3255bf20de0d59ee
mathvolcano <mathvolcano@users.noreply.github.com>
2022-05-29 12:49:57 -0700

    Add fast & slow solution to 0142_detectCycle.py

--- 0142_detectCycle.py
+++ 0142_detectCycle.py
@@ -11,16 +11,38 @@ https://leetcode.com/problems/linked-list-cycle-ii/
 
 class Solution:
     def detectCycle(self, head: ListNode) -> ListNode:
+        # Fast & slow pointers
+        # [1] get the length of the cycle by using a fast & slow pointer method
+        # [2] initialize 2 pointers at head, p1 = p2 = head. Move p2 forward k spaces
+        # [3] increment both pointers until p1 == p2 & return
+        # O(n) time and O(1) space, where n = length(head)
+        def length_of_cycle(head):
+            f = s = head
+            while f and f.next:
+                f = f.next.next
+                s = s.next
+                if f == s:
+                    return get_length(s)
+            return 0
 
-        slow = fast = head
-        while fast and fast.next:
-            slow, fast = slow.next, fast.next.next
+        def get_length(s):
+            c = s  # current
+            length = 0
+            while True:
+                c = c.next
+                length += 1
+                if c == s:
+                    break
+            return length
 
-            if slow is fast:
-                node = head
-                while node is not slow:
-                    node = node.next
-                    slow = slow.next
-                return node
-        return None
-
+        p = head
+        len_cycle = length_of_cycle(p)
+        if len_cycle == 0: return None
+        p1 = p2 = head
+        while len_cycle:
+            p2 = p2.next
+            len_cycle -= 1
+        while p1 != p2:
+            p1 = p1.next
+            p2 = p2.next
+        return p1
