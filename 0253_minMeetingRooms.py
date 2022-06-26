4236dfff7af66ba2b78a20dc461eda458abcd0dc
mathvolcano <mathvolcano@users.noreply.github.com>
2022-06-26 15:11:52 -0700

    Refactor 0253_minMeetingRooms

--- 0253_minMeetingRooms.py
+++ 0253_minMeetingRooms.py
@@ -5,38 +5,26 @@ https://leetcode.com/problems/meeting-rooms-ii/
 
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-
-        # Heap solution O(n log n)
-        from heapq import heappushpop, heappush
-        if len(intervals) == 1: return 1
-
-        size = len(intervals)
-        if size <= 1: return size
-        heap = []
-        for i in sorted(intervals):
-            if heap and i[0] >= heap[0]:
-                heappushpop(heap,i[1])
-            else:
-                heappush(heap,i[1])
-        return len(heap)
-
-        # # 1. Sort intervals by start time
-        # # 2. Have a hall of rooms, exit hall after time has elapsed
-        # # 3. Return the length of the hall
-        # # O(n log n) time needed for sort. O(n) space needed
-        #
-        if len(intervals) == 1: return 1
-        # intervals.sort(key=lambda x: x[0])
-        # hall = [[]]
-        # for j, span in enumerate(intervals):
-        #     found = 0
-        #     for i, room in enumerate(hall):
-        #         if (len(room) == 0) or (span[0] >= room[1]):
-        #             hall[i] = span
-        #             found = 1
-        #             break
-        #     if found == 0:
-        #         hall = hall + [span]
-        # return len(hall)
+        # [1] Sort meeings
+        # [2] Initialize a result variable res = 1 and a min heap, h = []
+        # [3] Iterate m through intervals
+        # [4] while h and m starts after the first element of h ends, heappop(h)
+        # [5] else, heappush(h, m.end)
+        # [3] Update result object as length of heap
+        # [4] return res
+        # Complexity:
+        # Time: O(n log n) time complexity for sort, iterations & heaps are at worst O( n * log n) => Final O( n lg n)
+        # Space: O(n) worst case when all meetings have conflicts to store in heap h, but can make additional space O(1) by popping meetings
+        # but would still need O(n) space for the sorting operation.
+        from heapq import heappop, heappush
+        n = len(intervals)
+        if n <= 1: return n
+        res, h = 0, []
+        for m in sorted(intervals):  # meeting
+            while h and m[0] >= h[0]:
+                heappop(h)
+            heappush(h, m[1])
+            res = max(res, len(h))
+        return res
 
 
