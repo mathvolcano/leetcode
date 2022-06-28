5864bd28f43d7e7821aadcb38616ce9cc976e672
mathvolcano <mathvolcano@users.noreply.github.com>
2022-06-27 17:13:34 -0700

    Add O(1) space solution to 0268_missingNumber.py

--- 0268_missingNumber.py
+++ 0268_missingNumber.py
@@ -1,29 +1,40 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Tue Mar 10 17:55:55 2020
-
-@author: mathvolcano
-
 268. Missing Number
 https://leetcode.com/problems/missing-number/
 """
 
-def missingNumber(nums):
-    all_ints = list(range(len(nums)+1))
-    
-    for n in nums:
-        all_ints[n] = -1
-        
-    for n in all_ints:
-        if n >= 0:
-            return n
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
 
-nums = [3,0,1]
-missingNumber(nums) # 2
+        # Cyclic sort
+        # [1] extend nums with new element -1
+        # [2] loop i through nums
+        # [3] if nums[i] == -1 or i != nums[i], then swap nums[i] and nums[nums[i]], else incrmeent i
+        # [4] loop through nums again and return the index of the -1
+        # O(n) time and O(1) space
+        nums.append(-1)
+        i, n = 0, len(nums)
+        while i < n:
+            j = nums[i]
+            if j == -1 or i == j:
+                i += 1
+            else:
+                nums[i], nums[j] = nums[j], nums[i]
 
-nums = [9,6,4,2,3,5,7,0,1]
-missingNumber(nums) # 8
+        for i, v in enumerate(nums):
+            if v == -1:
+                return i
 
-nums = [1]
-missingNumber(nums) # 0
+        # Array copy
+        # [1] Create an array of all integers in order, all_ints
+        # [2] For n in nums set, set all_ints[n] = -1
+        # [3] Loop through all_ints and return the first element that is not -1
+        # O(n) time and O(n) space
+#         all_ints = list(range(len(nums)+1))
+
+#         for n in nums:
+#             all_ints[n] = -1
+
+#         for n in all_ints:
+#             if n >= 0:
+#                 return n
