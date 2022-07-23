0e0207d144645ac9660cde26b95d07eebae6a242
mathvolcano <mathvolcano@users.noreply.github.com>
2022-07-23 15:52:09 -0700

    Update 0680_validPalindrome to make optimal.

--- 0680_validPalindrome.py
+++ 0680_validPalindrome.py
@@ -3,18 +3,30 @@
 https://leetcode.com/problems/valid-palindrome-ii/
 """
 
-def validPalindrome(s):
-    def is_palindrome(s):
-        return s == s[::-1]
-    
-    if len(s) <= 1:
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        # 2 pointers
+        # [1] initialize l, r = 0, len(s) - 1
+        # [2] if s[l] != s[r], check if s[l:r] is palindrome or s[l+1:r+1] is a palindrome and return
+        # [3] must define a helper function to do that without storing extra copy of s
+        # [4] else increment l & decrement r
+        # Complexity: n = len(s)
+        # Time: O(n) time
+        # Space: O(1) for storing pointers
+
+        def helper(s, p, q):
+            while p < q:
+                if s[p] != s[q]:
+                    return False
+                p += 1
+                q -= 1
+            return True
+
+        l, r = 0, len(s) -1
+        while l < r:
+            if s[l] != s[r]:
+                return helper(s, l+1, r) or helper(s,l, r-1)
+            l += 1
+            r -= 1
         return True
-    
-    l = 0
-    r = len(s)-1
-    while l < r:
-        if s[l] != s[r]:
-            return is_palindrome(s[:l] + s[l+1:]) | is_palindrome(s[:r] + s[r+1:])
-        l += 1
-        r -= 1
-    return True
+
