2a4b514caab2d6f29b3cb3e498cd34052d1dce74
mathvolcano <mathvolcano@users.noreply.github.com>
2022-08-01 07:19:32 -0700

    Add BFS solution to 0046_permute.py

--- 0046_permute.py
+++ 0046_permute.py
@@ -11,18 +11,38 @@ class Solution:
         # [2] Perform a DFS iterating through nums
         # O(n!) time and space.
 
-        def helper(arr, current, permutations):
-            if not len(arr) and len(current):
-                permutations.append(current)
-            else:
-                for i in range(len(arr)):
-                    newArr = arr[:i] + arr[i+1:]
-                    newCurr = current + [arr[i]]
-                    helper(newArr, newCurr, permutations)
+        # def helper(arr, current, permutations):
+        #     if not len(arr) and len(current):
+        #         permutations.append(current)
+        #     else:
+        #         for i in range(len(arr)):
+        #             newArr = arr[:i] + arr[i+1:]
+        #             newCurr = current + [arr[i]]
+        #             helper(arr, newCurr, permutations)
+        # result = []
+        # helper(nums, [], result)
+        # return result
 
-        result = []
-        helper(nums, [], result)
-        return result
+        # [0] Initialize result object, res = []
+        # [1] Iterate through each value of nums and, for every element already in result, insert
+        # the value into every position
+        # Complexity: n = len(nums)
+        # Time: O(n * n!) because each element of result has length at most n for inserting elements
+        # and the number of permutations is order n!.
+        # Space: O(n * n!) because each element of result has length n and the number of
+        # permutations is order of n!.
+        res = []
+        for n in nums:
+            if len(res) == 0: res.append([n])
+            else:
+                lvl = []
+                for i, e in enumerate(res):
+                    for j in range(len(e)+1):
+                        k = e.copy()
+                        k.insert(j, n)
+                        lvl.append(k)
+                res = lvl
+        return res
 
 
 
