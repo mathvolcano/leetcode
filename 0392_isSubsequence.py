1b1f00a5bc1fa5c0380a71db1a6744ae0aaff3b8
mathvolcano <mathvolcano@users.noreply.github.com>
2022-11-08 10:10:58 -0800

    Clean up 0392_isSubsequence

--- 0392_isSubsequence.py
+++ 0392_isSubsequence.py
@@ -5,14 +5,17 @@ https://leetcode.com/problems/is-subsequence/
 
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        """O(len(s) + len(t))"""
+        #2 pointers
+        # Complexity: n = len(s) and m = len(t)
+        # Time: O(n+m) worst case when we must traverse entirety of both strings
+        # Space: O(1) for pointers
         if not s: return True
         if not t or (len(s) > len(t)): return False
 
-        n_chars = 0
+        p = 0
         for c in t:
-            if c == s[n_chars]:
-                n_chars += 1
-            if n_chars == len(s):
+            if c == s[p]:
+                p += 1
+            if p == len(s):
                 break
-        return n_chars == len(s)
+        return p == len(s)
